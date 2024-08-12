53894c107e7ef36bb02ecf1a619541fa77dbd905
juharmansah <juharmansah0603@gmail.com>
2024-08-12 14:20:55 +0700

    push

---
+++ routes/routes.js
@@ -0,0 +1,7 @@
+import express from 'express';
+import { homeController } from '../controllers/homeController.js';
+const routes = express.Router();
+
+routes.get('/', homeController);
+
+export default routes;
