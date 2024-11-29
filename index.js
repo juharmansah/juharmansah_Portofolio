accb5e898d51fcb9d7780df40dcf6d251cc4be56
juharmansah <juharmansah0603@gmail.com>
2024-11-30 06:54:45 +0700

    save

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
 import express from "express";
 import routes from "./routes/routes.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+// Untuk mendukung __dirname di ES Module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-const port = process.env.PORT ||3000
+const port = process.env.PORT || 3000;
 
-//setup static files
-app.use(express.static(path.join(process.cwd(), 'public')));
+// Setup static files
+app.use(express.static(path.join(__dirname, 'public')));
 
-//ejs setup
+// EJS setup
 app.set('view engine', 'ejs');
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 
-//create routes
-app.use('/', routes)
+// Create routes
+app.use('/', routes);
 
+// Start server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
+// //Setiap kali kamu membuat perubahan pada aplikasi dan ingin memperbarui versi yang di-hosting di Vercel, 
+// cukup jalankan perintah:
 
-app.listen(port, ()=>{
-    console.log('server is running on port ${port}');
-})
+// vercel --prod
